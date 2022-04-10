fe83cdb59f522450151ca19a2b56bba05afdafc9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-04-10 14:35:33 +0300

    feat: add ESLint and Prettier and make them work together

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() : JSX.Element {
-  return (
-    <div className="App">
-      <h1>Instagram-clone</h1>
-      <h1>Instagram</h1>
-    </div>
-  );
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <h1>Instagram-clone</h1>
+            <h1>Instagram</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
