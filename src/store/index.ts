485217520a6cd8ea505bb601bde43a736545463b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-04-16 21:35:02 +0300

    fix: fixed the redux store to work fine with redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
-import { persistReducer } from 'redux-persist'
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import authReducer from './auth/authSlice'
 
@@ -17,7 +25,18 @@ const persistedReducer = persistReducer(persistConfig, reducers)
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({ serializableCheck: false }),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
