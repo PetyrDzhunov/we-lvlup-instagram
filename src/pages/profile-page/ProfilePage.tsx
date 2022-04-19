8a5b0fb8ebb117a7e21c5bd35614cf8287b755f9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-04-19 16:07:56 +0300

    feat: better UI/UX for profile page with skeletons

--- src/pages/profile-page/ProfilePage.tsx
+++ src/pages/profile-page/ProfilePage.tsx
@@ -1,17 +1,21 @@
 import Box from '@mui/material/Box'
+import Skeleton from '@mui/material/Skeleton'
 import { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { useNavigate } from 'react-router-dom'
+import Stack from '@mui/material/Stack'
 import SinglePostImage from '../../components/SinglePostImage'
 import { useAppSelector } from '../../hooks/redux-hooks'
 import PageLayout from '../../layout/PageLayout/PageLayout'
 import { firebaseService } from '../../services/firebase-service'
 import { PageProps, Post } from '../../types'
 import ProfilePageHeader from './ProfilePageHeader'
+import '../../styles/single-post.css'
 
 function ProfilePage({ title }: PageProps): JSX.Element {
     const navigate = useNavigate()
     const [currentUserPosts, setCurrentUserPosts] = useState<Post[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     const { isAuthenticated, uid, email, fullName } = useAppSelector(
         (state) => state.persistedReducer.auth
@@ -19,12 +23,14 @@ function ProfilePage({ title }: PageProps): JSX.Element {
 
     useEffect(() => {
         const getMyPosts = async (): Promise<void> => {
+            setIsLoading(true)
             try {
                 const posts = await firebaseService.getAllPostsByUserID(uid)
                 setCurrentUserPosts(posts)
             } catch (err) {
                 console.log(err)
             }
+            setIsLoading(false)
         }
         getMyPosts()
     }, [uid])
@@ -64,6 +70,39 @@ function ProfilePage({ title }: PageProps): JSX.Element {
                     ))}
                 </Box>
             </Box>
+            {isLoading && (
+                <Stack
+                    sx={{ marginLeft: '15px' }}
+                    spacing={2}
+                    direction="column"
+                    justifyContent="center"
+                >
+                    <Stack direction="row" spacing={2}>
+                        <Skeleton
+                            variant="rectangular"
+                            width="40%"
+                            height={120}
+                        />
+                        <Skeleton
+                            variant="rectangular"
+                            width="40%"
+                            height={120}
+                        />
+                    </Stack>
+                    <Stack direction="row" spacing={2}>
+                        <Skeleton
+                            variant="rectangular"
+                            width="40%"
+                            height={120}
+                        />
+                        <Skeleton
+                            variant="rectangular"
+                            width="40%"
+                            height={120}
+                        />
+                    </Stack>
+                </Stack>
+            )}
         </PageLayout>
     )
 }
