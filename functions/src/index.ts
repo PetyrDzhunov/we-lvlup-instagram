01820539f7ac71d6d198f4478d3788a0e60768fe
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-05-11 14:38:02 +0300

    feat: cron job with firebase finished - story is deleted automatically 24 hours after being created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,24 @@
 import functions = require('firebase-functions')
-
 import admin = require('firebase-admin')
 
 admin.initializeApp()
-const database = admin.firestore()
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+
+const firestore = admin.firestore()
 
 exports.scheduledFunction = functions.pubsub
-    .schedule('every 30 minutes')
-    .onRun((context) => {
-        // map through the users
-        // check if the date now is higher than the story expiresAt prop
-        // if its higher delete it
-        const users = database.collection('users')
-
-        // if(admin.firestore.Timestamp.now() > )
+    .schedule('every 10 minutes')
+    .onRun(async () => {
+        const users = firestore.collection('users')
+        const snapshot = await users.get()
+        snapshot.forEach(async (user) => {
+            const document = firestore.doc(`users/${user.id}`)
+            const data = user.data()
+            if (admin.firestore.Timestamp.now() > data.story.expiresAt) {
+                await document.update({
+                    story: admin.firestore.FieldValue.delete(),
+                })
+            }
+        })
 
         return null
     })
